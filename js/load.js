2d4b799c814474fab7a5122682f4e084ab185dd2
ZY <zzymoon@gmail.com>
2020-08-15 22:33:38 +0800

    updated

---
+++ js/load.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+
+            var imgCon = document.getElementsByClassName('card-columns')
+
+            var imgDivs = imgCon[0].getElementsByTagName('div')
+
+
+
+            for (var i = 0; i < imgDivs.length; i++) {
+
+                var img = imgDivs[i].getElementsByTagName('img')[0];
+
+                (function(curImg) {
+
+                    var tempImg = null;
+                    tempImg = document.createElement('img');
+
+                    tempImg.src = curImg.dataset.src;
+                    tempImg.onload = function(e) {
+                        curImg.src = tempImg.src;
+                        curImg.style.filter = 'blur(0px)';
+                    }
+                })(img)
+            }
+        }
