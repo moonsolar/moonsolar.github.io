b6f1a87a29725758551a3f9798451cb6bcdbd8dd
ZY <zzymoon@gmail.com>
2020-08-17 16:31:20 +0800

    updated

--- js/load.js
+++ js/load.js
@@ -1,42 +1,38 @@
 window.onload = function() {
+    var imgCon = document.getElementsByClassName('card-columns')
+    var imgDivs = imgCon[0].getElementsByTagName('div')
 
-            var imgCon = document.getElementsByClassName('card-columns')
+    for (var i = 0; i < imgDivs.length; i++) {
 
-            var imgDivs = imgCon[0].getElementsByTagName('div')
+        var img = imgDivs[i].getElementsByTagName('img')[0];
 
+        (function(curImg) {
 
+            var tempImg = null;
+            tempImg = document.createElement('img');
 
-            for (var i = 0; i < imgDivs.length; i++) {
-
-                var img = imgDivs[i].getElementsByTagName('img')[0];
-
-                (function(curImg) {
-
-                    var tempImg = null;
-                    tempImg = document.createElement('img');
-
-                    tempImg.src = curImg.dataset.src;
-                    tempImg.onload = function(e) {
-                        curImg.src = tempImg.src;
-                        curImg.style.filter = 'blur(0px)';
-                    }
-                })(img)
+            tempImg.src = curImg.dataset.src;
+            tempImg.onload = function(e) {
+                curImg.src = tempImg.src;
+                curImg.style.filter = 'blur(0px)';
             }
-        }
+        })(img)
+    }
+}
 
-        $('[data-magnify]').magnify({
-          headToolbar: [
-            'close'
-          ],
-          footToolbar: [
-            'zoomIn',
-            'zoomOut',
-            'prev',
-            'fullscreen',
-            'next',
-            'actualSize',
-            'rotateRight'
-          ],
-          title: true
-          // initMaximized: true
-        });
+$('[data-magnify]').magnify({
+    headToolbar: [
+        'close'
+    ],
+    footToolbar: [
+        'zoomIn',
+        'zoomOut',
+        'prev',
+        'fullscreen',
+        'next',
+        'actualSize',
+        'rotateRight'
+    ],
+    title: true
+    // initMaximized: true
+});
